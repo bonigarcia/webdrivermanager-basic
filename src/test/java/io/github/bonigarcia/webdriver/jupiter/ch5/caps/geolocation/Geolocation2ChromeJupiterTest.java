5a0c601212b9d3081c7e290e3dc7b3c1c5ef1ceb
Boni García <boni.garcia@uc3m.es>
2021-09-17 15:44:12 +0200

    Include manual wait in geolocation test

--- src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/Geolocation2ChromeJupiterTest.java
+++ src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/Geolocation2ChromeJupiterTest.java
@@ -61,9 +61,10 @@ class Geolocation2ChromeJupiterTest {
     }
 
     @Test
-    void testGeolocation() {
+    void testGeolocation() throws InterruptedException {
         driver.get("https://www.w3schools.com/html/html5_geolocation.asp");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Thread.sleep(Duration.ofSeconds(3).toMillis());
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.id("accept-choices")).click();
         driver.findElement(By.cssSelector("#main > p:nth-child(10) > button"))
                 .click();
