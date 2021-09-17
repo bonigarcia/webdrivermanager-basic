ed45783c46a37576afc3509ec44d51425961e258
Boni García <boni.garcia@uc3m.es>
2021-09-17 15:55:04 +0200

    Remove second geolocation test

--- src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/GeolocationChromeJupiterTest.java
+++ src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/GeolocationChromeJupiterTest.java
@@ -50,17 +50,13 @@ class GeolocationChromeJupiterTest {
         ChromeOptions options = new ChromeOptions();
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.default_content_setting_values.geolocation", 1);
-
         options.setExperimentalOption("prefs", prefs);
 
         driver = WebDriverManager.chromedriver().capabilities(options).create();
     }
 
     @AfterEach
-    void teardown() throws InterruptedException {
-        // FIXME: pause for manual browser inspection
-        Thread.sleep(Duration.ofSeconds(3).toMillis());
-
+    void teardown() {
         driver.quit();
     }
 
@@ -68,11 +64,10 @@ class GeolocationChromeJupiterTest {
     void testGeolocation() {
         driver.get(
                 "https://bonigarcia.dev/selenium-webdriver-java/geolocation.html");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         driver.findElement(By.id("get-coordinates")).click();
 
         WebElement coordinates = driver.findElement(By.id("coordinates"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(coordinates));
 
         TakesScreenshot ts = (TakesScreenshot) driver;
@@ -80,6 +75,7 @@ class GeolocationChromeJupiterTest {
         log.debug("Screenshot in base64 "
                 + "(you can copy and paste it into a browser navigation bar to watch it)\n"
                 + "data:image/png;base64,{}", screenshot);
+
         assertThat(coordinates.getText()).contains("Latitude")
                 .contains("Longitude");
     }
