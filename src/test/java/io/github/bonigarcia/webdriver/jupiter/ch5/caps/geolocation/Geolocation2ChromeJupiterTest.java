98524ec13f8e1dc3e2e093b939289748c7a3d10b
Boni García <boni.garcia@uc3m.es>
2021-09-17 15:08:28 +0200

    Include new test using geolocation

--- src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/Geolocation2ChromeJupiterTest.java
+++ src/test/java/io/github/bonigarcia/webdriver/jupiter/ch5/caps/geolocation/Geolocation2ChromeJupiterTest.java
@@ -16,6 +16,8 @@
  */
 package io.github.bonigarcia.webdriver.jupiter.ch5.caps.geolocation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,7 +34,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-class GeolocationChromeJupiterTest {
+class Geolocation2ChromeJupiterTest {
 
     WebDriver driver;
 
@@ -57,13 +59,15 @@ class GeolocationChromeJupiterTest {
 
     @Test
     void testGeolocation() {
-        driver.get("https://the-internet.herokuapp.com/geolocation");
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/geolocation.html");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        driver.findElement(By.xpath("//*[@id='content']/div/button")).click();
-        WebElement coordinates = driver.findElement(By.id("demo"));
-        wait.until(ExpectedConditions.textToBePresentInElement(coordinates,
-                "Latitude"));
+        driver.findElement(By.id("get-coordinates")).click();
+        WebElement coordinates = driver.findElement(By.id("coordinates"));
+        wait.until(ExpectedConditions.visibilityOf(coordinates));
+        assertThat(coordinates.getText()).contains("Latitude")
+                .contains("Longitude");
     }
 
 }
