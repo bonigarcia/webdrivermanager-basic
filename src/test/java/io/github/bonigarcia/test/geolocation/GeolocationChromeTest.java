e0677729da0ad5c9328630bbdb0a22fe8cc73aca
Boni García <boni.garcia@uc3m.es>
2021-10-02 02:15:52 +0200

    Disable geolocation test using Chrome

--- src/test/java/io/github/bonigarcia/test/geolocation/GeolocationChromeTest.java
+++ src/test/java/io/github/bonigarcia/test/geolocation/GeolocationChromeTest.java
@@ -26,6 +26,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -39,6 +40,7 @@ import org.slf4j.Logger;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@Disabled
 class GeolocationChromeTest {
 
     static final Logger log = getLogger(lookup().lookupClass());
